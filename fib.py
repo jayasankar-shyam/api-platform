92555b4e7874d8573ef7f2359adcf47af133c4ed
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2025-04-16 12:41:42 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -6,12 +6,3 @@ def recur_fibo(n):
    else:
        return(recur_fibo(n-1) + recur_fibo(n-2))
 
-nterms = 10
-
-# check if the number of terms is valid
-if nterms <= 0:
-   print("Plese enter a positive integer")
-else:
-   print("Fibonacci sequence:")
-   for i in range(nterms):
-       print(recur_fibo(i))
